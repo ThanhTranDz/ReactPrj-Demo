76b20b14c51e5045a3106eb2c808d6fd13cd2dd2
Thanh Tran <you@example.com>
2025-04-01 15:18:42 +0700

    Fix vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
   plugins: [
     react(), 
-    eslint(),
+    eslintPlugin(),
     tailwindcss(),
   ],
 });
